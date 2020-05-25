a2a02a25d814c6a25e843178d3cca9db922780d6
Zareen Price <yazatamorph@gmail.com>
2020-05-25 15:21:56 -0400

    Small adjustment to header and footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,9 @@ class Footer extends React.Component {
   render() {
     return (
       <div className="app-footer">
-        <p>An app by Zareen Price</p>
+        <p>
+          An app by <a href="https://github.com/yazatamorph">Zareen Price</a>
+        </p>
       </div>
     );
   }
